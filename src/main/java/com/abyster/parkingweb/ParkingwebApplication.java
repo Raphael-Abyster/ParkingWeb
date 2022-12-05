5cce0453864d43d470eee5450e75ea8617ee3e48
Raphael-Abyster <raphael.de-castro@abyster.com>
2022-12-05 13:27:33 +0100

    Initialisation

---
+++ src/main/java/com/abyster/parkingweb/ParkingwebApplication.java
@@ -0,0 +1,13 @@
+package com.abyster.parkingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingwebApplication.class, args);
+	}
+
+}
